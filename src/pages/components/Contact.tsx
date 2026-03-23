65cb7d75b08e5019206e9d4400e44cb14c9eadd6
JJamC <jaycarroll97@live.co.uk>
2026-03-23 17:26:33 +0000

    restyled contact form

--- src/pages/components/Contact.tsx
+++ src/pages/components/Contact.tsx
@@ -21,36 +21,68 @@ export default function ContactForm() {
   }
 
   return (
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20 place-items-center">
+    <div>
+      <p>Want to work with me?</p>
+      <p>Get in touch :) </p>
+      <form
+        action="#"
+        method="POST"
+        className="mx-auto max-w-xl place-items-center"
+      >
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label className="block text-sm/6 font-semibold text-gray-900">First name</label>
+            <label className="block text-sm/6 font-semibold">First name</label>
             <div className="mt-2.5">
-              <input id="first-name" type="text" name="first-name" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+              <input
+                id="first-name"
+                type="text"
+                name="first-name"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
             </div>
           </div>
           <div>
-            <label className="block text-sm/6 font-semibold text-gray-900">Last name</label>
+            <label className="block text-sm/6 font-semibold">Last name</label>
             <div className="mt-2.5">
-              <input id="last-name" type="text" name="last-name" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+              <input
+                id="last-name"
+                type="text"
+                name="last-name"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label className="block text-sm/6 font-semibold text-gray-900">Email</label>
+            <label className="block text-sm/6 font-semibold">Email</label>
             <div className="mt-2.5">
-              <input id="email" type="email" name="email" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+              <input
+                id="email"
+                type="email"
+                name="email"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label className="block text-sm/6 font-semibold text-gray-900">Message</label>
+            <label className="block text-sm/6 font-semibold">Message</label>
             <div className="mt-2.5">
-              <textarea id="message" name="message" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+              <textarea
+                id="message"
+                name="message"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              ></textarea>
             </div>
           </div>
         </div>
         <div className="mt-10">
-          <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send</button>
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-[#FC834A] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-[#F14F04]"
+          >
+            Send
+          </button>
         </div>
       </form>
-  )
+    </div>
+  );
 }
